5c3793acaf3461ae0d76f559d3a85af7be39944d
Germanus <germanus@github.com>
2015-03-02 15:59:06 +0300

    css

--- src/main/java/my/ilya/jsf/core/Word.java
+++ src/main/java/my/ilya/jsf/core/Word.java
@@ -1,13 +1,17 @@
 package my.ilya.jsf.core;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Word {
+public class Word implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
